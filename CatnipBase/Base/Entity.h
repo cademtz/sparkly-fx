89e1f7102642846af744504a502d30ee00bb07ff
Hold on! <know.one.will.find.out@gmail.com>
2020-06-18 01:12:45 -0500

    Fixed borked netvars and steam overlay crashing

--- CatnipBase/Base/Entity.h
+++ CatnipBase/Base/Entity.h
@@ -2,10 +2,12 @@
 #include "Netvars.h"
 #include "SDK/vector.h"
 
-#define NV_RET(type, var) return *(type*)((char*)this + var)
+#define NV_RET(type, var) return *(type*)(this + var)
 
 class CBaseEntity
 {
 public:
-	inline Vector Origin() { NV_RET(Vector, Netvars::baseentity.m_vecOrigin); }
+	inline Vector Origin() { NV_RET(Vector, Netvars::m_vecOrigin); }
+	inline QAngle Rotation() { NV_RET(QAngle, Netvars::m_angRotation); }
+	inline int Team() { NV_RET(int, Netvars::m_iTeamNum); }
 };
--- CatnipBase/Base/Netvars.h
+++ CatnipBase/Base/Netvars.h
@@ -5,9 +5,9 @@ namespace Netvars
 {
 	void GetNetvars();
 
-	inline struct {
-		int m_vecOrigin;
-		int m_angRotation;
-		int m_iTeamNum;
-	} baseentity;
+	inline int m_vecOrigin = 0;
+	inline int m_angRotation = 0;
+	inline int m_iTeamNum = 0;
+	inline int m_iHealth = 0;
+	inline int m_lifeState = 0;
 }
