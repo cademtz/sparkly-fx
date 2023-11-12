741edfdfc5a9736e424818b2b1c62ce92700b6be
cademtz <cadeemz@outlook.com>
2023-11-11 23:36:50 -0600

    Update IMGUI, IMaterialSystemWrapper, and remove the `nullptr` macro from some Source SDK header

--- src/Base/Wrappers/MaterialSystemWrappers.cpp
+++ src/Base/Wrappers/MaterialSystemWrappers.cpp
@@ -8,11 +8,34 @@ IMatRenderContext* IMaterialSystemWrapperSDK::GetRenderContext() {
 IMaterial* IMaterialSystemWrapperSDK::CreateMaterial(const char *pMaterialName, KeyValues *pVMTKeyValues) {
     return m_int->CreateMaterial(pMaterialName, pVMTKeyValues);
 }
-
+MaterialHandle_t IMaterialSystemWrapperSDK::FirstMaterial() {
+    return m_int->FirstMaterial();
+}
+MaterialHandle_t IMaterialSystemWrapperSDK::NextMaterial(MaterialHandle_t handle) {
+    return m_int->NextMaterial(handle );
+}
+MaterialHandle_t IMaterialSystemWrapperSDK::InvalidMaterial() {
+    return m_int->InvalidMaterial();
+}
+IMaterial* IMaterialSystemWrapperSDK::GetMaterial(MaterialHandle_t handle) {
+    return m_int->GetMaterial(handle);
+}
 
 IMatRenderContext* IMaterialSystemWrapper081::GetRenderContext() {
     return m_int->GetRenderContext();
 }
 IMaterial* IMaterialSystemWrapper081::CreateMaterial(const char *pMaterialName, KeyValues *pVMTKeyValues) {
     return m_int->CreateMaterial(pMaterialName, pVMTKeyValues);
+}
+MaterialHandle_t IMaterialSystemWrapper081::FirstMaterial() {
+    return m_int->FirstMaterial();
+}
+MaterialHandle_t IMaterialSystemWrapper081::NextMaterial(MaterialHandle_t handle) {
+    return m_int->NextMaterial(handle );
+}
+MaterialHandle_t IMaterialSystemWrapper081::InvalidMaterial() {
+    return m_int->InvalidMaterial();
+}
+IMaterial* IMaterialSystemWrapper081::GetMaterial(MaterialHandle_t handle) {
+    return m_int->GetMaterial(handle);
 }
