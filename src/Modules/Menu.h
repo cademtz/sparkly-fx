fb92cc3d7d24ffa61fddef105f77c7d656bbd07a
cademtz <cadeemz@outlook.com>
2023-11-16 16:10:53 -0600

    Fix OverrideView hook not calling original. Add EVENT_POST_IMGUI_INPUT.

--- src/Modules/Menu.h
+++ src/Modules/Menu.h
@@ -3,22 +3,22 @@
 #include <imgui.h>
 
 DECL_EVENT(EVENT_MENU);
+/// @brief This runs after IMGUI reads input. It can also hide input from the game.
+DECL_EVENT(EVENT_POST_IMGUI_INPUT);
 
 class CMenu : public CModule, CEventManager
 {
 public:
 	CMenu();
 	void StartListening() override;
-	bool IsOpen();
 	void SetOpen(bool Val);
+	bool IsOpen();
 
 private:
 	bool AcceptMsg(HWND hWnd, UINT uMsg, LPARAM lParam, WPARAM wParam);
 
 	int OnImGui();
 	int OnWindowProc();
-
-
 
 	bool m_open = false;
 };
