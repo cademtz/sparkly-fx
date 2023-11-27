768dd8dfd575fef27071bdcc5eb5c49a956b09dd
cademtz <cadeemz@outlook.com>
2023-11-27 03:40:06 -0600

    Add `IVModelRenderGMod` class and its wrapper

--- src/Base/Wrappers/ModelRenderWrappers.h
+++ src/Base/Wrappers/ModelRenderWrappers.h
@@ -2,6 +2,7 @@
 #include "Wrappers.h"
 
 class IVModelRender;
+class IVModelRenderGMod;
 
 class IVModelRenderWrapperSDK : public IVModelRenderWrapper
 {
@@ -9,6 +10,19 @@ class IVModelRenderWrapperSDK : public IVModelRenderWrapper
 
 public:
     IVModelRenderWrapperSDK(void* instance) : m_int((IVModelRender*)instance) {}
+    void* Inst() override { return m_int; }
+    int GetOffset(EOffsets Offset) override;
+
+    void ForcedMaterialOverride(IMaterial* newMaterial, OverrideType_t nOverrideType) override;
+    void DrawModelExecute(const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld) override;
+};
+
+class IVModelRenderWrapperGMod : public IVModelRenderWrapper
+{
+    IVModelRenderGMod* m_int;
+
+public:
+    IVModelRenderWrapperGMod(void* instance) : m_int((IVModelRenderGMod*)instance) {}
     void* Inst() override { return m_int; }
     int GetOffset(EOffsets Offset) override;
 
