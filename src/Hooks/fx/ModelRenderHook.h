477f18735ae3dbf2ce424e45985484443c36e8ae
cademtz <cadeemz@outlook.com>
2023-11-27 03:57:52 -0600

    Move `ModelRenderHook` to fx folder. Trim unused hook and fix func prototype for x64.

--- src/Hooks/fx/ModelRenderHook.h
+++ src/Hooks/fx/ModelRenderHook.h
@@ -1,18 +1,13 @@
 #pragma once
-#include "Hooks.h"
+#include <Hooks/Hooks.h>
 #include <SDK/istudiorender.h>
 
-DECL_EVENT(EVENT_DRAW_PROP);
 DECL_EVENT(EVENT_PRE_DRAW_MODEL_EXECUTE);
 DECL_EVENT(EVENT_POST_DRAW_MODEL_EXECUTE);
 
 struct ModelRenderInfo_t;
-struct StaticPropRenderInfo_t;
 struct DrawModelState_t;
-struct ModelRenderInfo_t;
 
-typedef int(__thiscall* DrawModelExStaticPropFn_t)(void*, ModelRenderInfo_t& pInfo);
-typedef int(__thiscall* DrawStaticPropArrayFastFn_t)(void*, StaticPropRenderInfo_t* pProps, int count, bool bShadowDepth);
 typedef void (__thiscall* DrawModelExecuteFn_t)(void*, const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld);
 
 class CModelRenderHook : public CBaseHook
@@ -25,25 +20,19 @@ public:
     void Hook() override;
     void Unhook() override;
     Ctx* Context() { return &m_ctx; }
-    int DrawModelExStaticProp(ModelRenderInfo_t& pInfo);
     void DrawModelExecute(const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld);
 
 private:
-    static int __stdcall Hooked_DrawModelExStaticProp(ModelRenderInfo_t& pInfo);
-    static void __stdcall Hooked_DrawModelExecute(const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld);
+    static void __stdcall Hooked_DrawModelExecute(UNCRAP const DrawModelState_t& state, const ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld);
 
     struct Ctx
     {
-        union
+        struct
         {
-            struct { ModelRenderInfo_t* pInfo; } static_prop;
-            struct
-            {
-                const DrawModelState_t* state;
-                const ModelRenderInfo_t* pInfo;
-                matrix3x4_t* pCustomBoneToWorld;
-            } model_execute;
-        };
+            const DrawModelState_t* state;
+            const ModelRenderInfo_t* pInfo;
+            matrix3x4_t* pCustomBoneToWorld;
+        } model_execute;
     };
 
     CVMTHook m_hook_model_render;
