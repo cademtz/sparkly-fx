b7ac6e8dad5b3a4735ec0894779a20c6bf17ea4b
cademtz <cadeemz@outlook.com>
2023-11-27 03:06:57 -0600

    Add `MaterialSystemGmod` class and its wrapper

--- src/Base/Wrappers/MaterialSystemWrappers.cpp
+++ src/Base/Wrappers/MaterialSystemWrappers.cpp
@@ -1,6 +1,7 @@
 #include "MaterialSystemWrappers.h"
 #include <SDK/imaterialsystem.h>
 #include <SDK/InterfaceVersions/MaterialSystem081.h>
+#include <SDK/InterfaceVersions/MaterialSystemGMod.h>
 
 IMatRenderContext* IMaterialSystemWrapperSDK::GetRenderContext() {
     return m_int->GetRenderContext();
@@ -43,6 +44,28 @@ IMaterial* IMaterialSystemWrapper081::GetMaterial(MaterialHandle_t handle) {
     return m_int->GetMaterial(handle);
 }
 ITexture* IMaterialSystemWrapper081::FindTexture(char const* pTextureName, const char *pTextureGroupName, bool complain, int nAdditionalCreationFlags) {
+    return m_int->FindTexture(pTextureName, pTextureGroupName, complain, nAdditionalCreationFlags);
+}
+
+IMatRenderContext* IMaterialSystemWrapperGMod::GetRenderContext() {
+    return m_int->GetRenderContext();
+}
+IMaterial* IMaterialSystemWrapperGMod::CreateMaterial(const char *pMaterialName, KeyValues *pVMTKeyValues) {
+    return m_int->CreateMaterial(pMaterialName, pVMTKeyValues);
+}
+MaterialHandle_t IMaterialSystemWrapperGMod::FirstMaterial() {
+    return m_int->FirstMaterial();
+}
+MaterialHandle_t IMaterialSystemWrapperGMod::NextMaterial(MaterialHandle_t handle) {
+    return m_int->NextMaterial(handle );
+}
+MaterialHandle_t IMaterialSystemWrapperGMod::InvalidMaterial() {
+    return m_int->InvalidMaterial();
+}
+IMaterial* IMaterialSystemWrapperGMod::GetMaterial(MaterialHandle_t handle) {
+    return m_int->GetMaterial(handle);
+}
+ITexture* IMaterialSystemWrapperGMod::FindTexture(char const* pTextureName, const char *pTextureGroupName, bool complain, int nAdditionalCreationFlags) {
     return m_int->FindTexture(pTextureName, pTextureGroupName, complain, nAdditionalCreationFlags);
 }
 
