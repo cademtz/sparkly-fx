0e018688bddb96787bd440b1aa043033f70e4f08
cademtz <cadeemz@outlook.com>
2023-11-27 20:25:48 -0600

    Add a sigscan method to find the IDirect3DDevice9 vtable

--- src/Hooks/OverlayHook.h
+++ src/Hooks/OverlayHook.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Hooks.h"
 #include <d3d9.h>
+#include <string>
 
 DECL_EVENT(EVENT_DX9PRESENT);
 DECL_EVENT(EVENT_DX9RESET);
@@ -26,6 +27,20 @@ private:
 	static HRESULT WINAPI Hooked_Present(
 		IDirect3DDevice9* thisptr, const RECT* Src, const RECT* Dest, HWND Window, const RGNDATA* DirtyRegion
 	);
+
+	/**
+	 * @brief Get the device vtable by signature scanning
+	 * @param out_error A string to store the error message
+	 * @return `nullptr` on failure
+	 */
+	static void** GetDeviceVtable_SigScan(std::string* out_error);
+	/**
+	 * @brief Get the device vtable by creating a temporary device.
+	 * 
+	 * This will exit the program on failure. Use only as a last-resort.
+	 * @param out_error A string to store the error message
+	 */
+	static void** GetDeviceVtable_CreateDevice(std::string* out_error);
 };
 
 inline COverlayHook g_hk_overlay;
