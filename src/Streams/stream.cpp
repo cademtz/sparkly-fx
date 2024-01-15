9bd2092c672f9b3a3752ec4889eade97c765da7d
cademtz <cadeemz@outlook.com>
2024-01-15 02:51:06 -0600

    Replace the depth fog preset with a real depth preset

--- src/Streams/stream.cpp
+++ src/Streams/stream.cpp
@@ -1,6 +1,7 @@
 #include "stream.h"
 #include <nlohmann/json.hpp>
 #include "materials.h"
+#include <Shaders/shaders.h>
 #include <Base/Interfaces.h>
 #include <SDK/texture_group_names.h>
 
@@ -125,52 +126,16 @@ std::vector<Stream::ConstPtr> Stream::MakePresets()
 
         vec.emplace_back(std::move(matte));
     }
-    // Depth fog
+    // Depth
     {
-        Stream::Ptr depth = std::make_shared<Stream>("Depth fog");
-
-        auto entities = std::make_shared<ModelTweak>();
-        entities->render_effect = ModelTweak::MaterialChoice::CUSTOM;
-        entities->custom_material = CustomMaterial::GetSolid();
-        entities->color_multiply = {0,0,0,1};
-        depth->m_tweaks.emplace_back(std::move(entities));
-
-        // Make all materials black
-        auto materials = std::make_shared<MaterialTweak>();
-        materials->filter_choice = FilterChoice::WHITELIST;
-        materials->color_multiply = {0,0,0,1};
-        materials->props = true;
-        for (size_t i = 0; i < MaterialTweak::TEXTURE_GROUPS.size(); ++i)
-        {
-            const char* group = MaterialTweak::TEXTURE_GROUPS[i];
-            materials->groups[i] =  !strcmp(group, TEXTURE_GROUP_CLIENT_EFFECTS)
-                                    || !strcmp(group, TEXTURE_GROUP_WORLD)
-                                    || !strcmp(group, TEXTURE_GROUP_SKYBOX)
-                                    || !strcmp(group, TEXTURE_GROUP_DECAL);
-        }
-        depth->m_tweaks.push_back(std::move(materials));
-        
-        auto fog = std::make_shared<FogTweak>();
-        fog->fog_enabled = true;
-        fog->fog_color = {1,1,1};
-        fog->fog_start = 2048;
-        depth->m_tweaks.push_back(std::move(fog));
+        Stream::Ptr depth = std::make_shared<Stream>("Depth");
 
         auto cam = std::make_shared<CameraTweak>();
-        cam->hud = CameraTweak::HUD_DISABLED;
+        cam->pixel_shader = Shader::PixelShader::GetLoadedShader<Shader::DepthLinear>();
         depth->m_tweaks.emplace_back(std::move(cam));
 
         auto misc = std::make_shared<CommandTweak>();
-        misc->commands =
-            "r_shadows 0\n"
-            "r_skybox 0\n"
-            "r_3dsky 0\n"
-            "r_drawparticles 0\n"
-            "glow_outline_effect_enable 0\n" // Glow effect is TF2-specific
-            "r_cleardecals\n"
-            "r_drawviewmodel 0\n"
-            "mat_drawwater 0\n" // Hack: Water renders oddly, so just hide it
-            "r_screenoverlay off";
+        misc->commands = "r_drawvgui 0";
         depth->m_tweaks.push_back(std::move(misc));
         
         vec.emplace_back(std::move(depth));
