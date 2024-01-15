29982ac70d2922cfef8e80c7c923b4ace6a57554
cademtz <cadeemz@outlook.com>
2024-01-15 01:08:49 -0600

    Add D3D helpers to invert matrix and draw a screen-space textured rect

--- src/Helper/d3d9.cpp
+++ src/Helper/d3d9.cpp
@@ -169,4 +169,172 @@ const char* GetD3DFormatAsFFmpegPixFmt(D3DFORMAT fmt, bool skip_alpha)
     };
 }
 
+void Invert4x4Matrix(const float *m, float *out)
+{
+
+/* NB. OpenGL Matrices are COLUMN major. */
+//#define MAT(m,r,c) (m)[(c)*4+(r)]
+
+// Cade: DirectX matrices are ROW major.
+#define MAT(m,r,c) (m)[(r)*4+(c)]
+
+/* Here's some shorthand converting standard (row,column) to index. */
+#define m11 MAT(m,0,0)
+#define m12 MAT(m,0,1)
+#define m13 MAT(m,0,2)
+#define m14 MAT(m,0,3)
+#define m21 MAT(m,1,0)
+#define m22 MAT(m,1,1)
+#define m23 MAT(m,1,2)
+#define m24 MAT(m,1,3)
+#define m31 MAT(m,2,0)
+#define m32 MAT(m,2,1)
+#define m33 MAT(m,2,2)
+#define m34 MAT(m,2,3)
+#define m41 MAT(m,3,0)
+#define m42 MAT(m,3,1)
+#define m43 MAT(m,3,2)
+#define m44 MAT(m,3,3)
+
+    static const float _identity[] = {
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1
+    };
+
+    float det;
+    float d12, d13, d23, d24, d34, d41;
+    float tmp[16]; /* Allow out == in. */
+
+    /* Inverse = adjoint / det. (See linear algebra texts.)*/
+
+    /* pre-compute 2x2 dets for last two rows when computing */
+    /* cofactors of first two rows. */
+    d12 = (m31*m42-m41*m32);
+    d13 = (m31*m43-m41*m33);
+    d23 = (m32*m43-m42*m33);
+    d24 = (m32*m44-m42*m34);
+    d34 = (m33*m44-m43*m34);
+    d41 = (m34*m41-m44*m31);
+
+    tmp[0] =  (m22 * d34 - m23 * d24 + m24 * d23);
+    tmp[1] = -(m21 * d34 + m23 * d41 + m24 * d13);
+    tmp[2] =  (m21 * d24 + m22 * d41 + m24 * d12);
+    tmp[3] = -(m21 * d23 - m22 * d13 + m23 * d12);
+
+    /* Compute determinant as early as possible using these cofactors. */
+    det = m11 * tmp[0] + m12 * tmp[1] + m13 * tmp[2] + m14 * tmp[3];
+
+    /* Run singularity test. */
+    if (det == 0.0) {
+        /* printf("invert_matrix: Warning: Singular matrix.\n"); */
+        memcpy(out,_identity,16*sizeof(float));
+    }
+    else {
+        float invDet = 1.0 / det;
+        /* Compute rest of inverse. */
+        tmp[0] *= invDet;
+        tmp[1] *= invDet;
+        tmp[2] *= invDet;
+        tmp[3] *= invDet;
+        
+        tmp[4] = -(m12 * d34 - m13 * d24 + m14 * d23) * invDet;
+        tmp[5] =  (m11 * d34 + m13 * d41 + m14 * d13) * invDet;
+        tmp[6] = -(m11 * d24 + m12 * d41 + m14 * d12) * invDet;
+        tmp[7] =  (m11 * d23 - m12 * d13 + m13 * d12) * invDet;
+        
+        /* Pre-compute 2x2 dets for first two rows when computing */
+        /* cofactors of last two rows. */
+        d12 = m11*m22-m21*m12;
+        d13 = m11*m23-m21*m13;
+        d23 = m12*m23-m22*m13;
+        d24 = m12*m24-m22*m14;
+        d34 = m13*m24-m23*m14;
+        d41 = m14*m21-m24*m11;
+        
+        tmp[8] =  (m42 * d34 - m43 * d24 + m44 * d23) * invDet;
+        tmp[9] = -(m41 * d34 + m43 * d41 + m44 * d13) * invDet;
+        tmp[10] =  (m41 * d24 + m42 * d41 + m44 * d12) * invDet;
+        tmp[11] = -(m41 * d23 - m42 * d13 + m43 * d12) * invDet;
+        tmp[12] = -(m32 * d34 - m33 * d24 + m34 * d23) * invDet;
+        tmp[13] =  (m31 * d34 + m33 * d41 + m34 * d13) * invDet;
+        tmp[14] = -(m31 * d24 + m32 * d41 + m34 * d12) * invDet;
+        tmp[15] =  (m31 * d23 - m32 * d13 + m33 * d12) * invDet;
+        
+        memcpy(out, tmp, 16*sizeof(float));
+    }
+
+#undef m11
+#undef m12
+#undef m13
+#undef m14
+#undef m21
+#undef m22
+#undef m23
+#undef m24
+#undef m31
+#undef m32
+#undef m33
+#undef m34
+#undef m41
+#undef m42
+#undef m43
+#undef m44
+#undef MAT
+}
+
+void DrawTexturedRect(IDirect3DDevice9* device, std::array<float, 4> screen_rect, std::array<float, 4> texture_rect)
+{
+    // The following code is adapted from the Half-Life Advanced Effects project (advancedfx.org)
+
+    constexpr DWORD RECT_BUFFER_FVF = (D3DFVF_XYZRHW | D3DFVF_TEX1);
+    struct RectBufferVertex
+    {
+        float x, y, z, rhw;
+        float u, v;
+    };
+
+    device->SetRenderState(D3DRS_SRGBWRITEENABLE, FALSE);
+    device->SetRenderState(D3DRS_COLORWRITEENABLE, D3DCOLORWRITEENABLE_ALPHA | D3DCOLORWRITEENABLE_BLUE | D3DCOLORWRITEENABLE_GREEN | D3DCOLORWRITEENABLE_RED);
+    device->SetRenderState(D3DRS_SEPARATEALPHABLENDENABLE, FALSE);
+    device->SetVertexShader(NULL);
+    device->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+    device->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+    device->SetRenderState(D3DRS_ZFUNC, D3DCMP_ALWAYS); // redundant due to ZENABLE
+    device->SetRenderState(D3DRS_ZENABLE, D3DZB_FALSE);
+    device->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+    device->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+    device->SetRenderState(D3DRS_MULTISAMPLEANTIALIAS, FALSE);
+    device->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+    device->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_SELECTARG1);
+    device->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+    device->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_SELECTARG1);
+    device->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+    device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+    device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+    device->SetSamplerState(0, D3DSAMP_SRGBTEXTURE, FALSE);
+    device->SetDepthStencilSurface(nullptr);
+
+    D3DMATRIX proj_matrix = {
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1,
+    };
+
+    device->SetTransform(D3DTS_WORLD, &proj_matrix);
+    device->SetTransform(D3DTS_VIEW, &proj_matrix);
+    device->SetTransform(D3DTS_PROJECTION, &proj_matrix);
+
+    RectBufferVertex vertices[4] = {
+        {screen_rect[0] + screen_rect[2]*0, screen_rect[1] + screen_rect[3]*1, 0, 0, texture_rect[0] + texture_rect[2]*0, texture_rect[1] + texture_rect[3]*1},
+        {screen_rect[0] + screen_rect[2]*0, screen_rect[1] + screen_rect[3]*0, 0, 0, texture_rect[0] + texture_rect[2]*0, texture_rect[1] + texture_rect[3]*0},
+        {screen_rect[0] + screen_rect[2]*1, screen_rect[1] + screen_rect[3]*1, 0, 0, texture_rect[0] + texture_rect[2]*1, texture_rect[1] + texture_rect[3]*1},
+        {screen_rect[0] + screen_rect[2]*1, screen_rect[1] + screen_rect[3]*0, 0, 0, texture_rect[0] + texture_rect[2]*1, texture_rect[1] + texture_rect[3]*0},
+    };
+    device->SetFVF(RECT_BUFFER_FVF);
+    device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, &vertices, sizeof(vertices[0]));
+}
+
 }
--- src/Helper/d3d9.h
+++ src/Helper/d3d9.h
@@ -1,4 +1,5 @@
 #include "dxerr.h"
+#include <array>
 #include <cstdint>
 #include <d3d9.h>
 
@@ -39,4 +40,21 @@ bool GetD3DFormatInfo(D3DFORMAT fmt, D3DFORMAT_info* out_info);
 /// @return `nullptr` if no equivalent `pix_fmt` exists
 const char* GetD3DFormatAsFFmpegPixFmt(D3DFORMAT fmt, bool skip_alpha);
 
+/**
+ * @brief Invert a row-major (DirectX-style) matrix
+ * @param m A 4x4 matrix
+ * @param out Destination of the inverse 4x4 matrix. It can be the same as `m`.
+ */
+void Invert4x4Matrix(const float *m, float *out);
+
+/**
+ * @brief Draw a textured rectangle in screen-space.
+ * 
+ * Everything is unbound or modified except the pixel shader, shader constants, and textures.
+ * You will want to create a stateblock to restore everything after this call.
+ * 
+ * @param screen_rect X,Y,W,H screen rectangle. Units are in pixels, with (0,0) being the top-left.
+ * @param texture_rect X,Y,W,H texture rectangle. Units are within [0,1].
+ */
+void DrawTexturedRect(IDirect3DDevice9* device, std::array<float, 4> screen_rect, std::array<float, 4> texture_rect = {0,0,1,1});
 }
